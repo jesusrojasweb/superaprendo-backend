630bf669bf86aa92e783ab0c8c824bd7bff273ae
Jesús Rojas <jesusrojas.frontend@gmail.com>
2020-06-08 10:05:05 -0400

    Configuracion inicial
    Se inicio con la configuración inicial de platzi para empezar a customizar

---
+++ config/env.js
@@ -0,0 +1,20 @@
+// CONFIG
+process.env.PORT = 3000;
+process.env.CORS = "*";
+
+// MONGO
+// process.env.DB_USER=
+// process.env.DB_PASSWORD=
+// process.env.DB_HOST=
+// process.env.DB_NAME=
+
+// // USERS
+// process.env.DEFAULT_ADMIN_PASSWORD=
+// process.env.DEFAULT_USER_PASSWORD=
+
+// // AUTH
+// process.env.AUTH_JWT_SECRET=
+
+// // API KEYS
+// process.env.PUBLIC_API_KEY_TOKEN=
+// process.env.ADMIN_API_KEY_TOKEN=
